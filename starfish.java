a52dbda24bf4a049ee28c28d7a757f46bfbfc4da
Fahad Israr <fahad00cms@gmail.com>
2020-04-23 03:58:25 +0530

    scanningaall installed packages

--- starfish.java
+++ starfish.java
@@ -6,18 +6,46 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Objects;
+import java.util.Properties;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 class starfish{
     public static void main(String[]args)throws Exception{
         for(String s:args)
-        System.out.println(s);//Printing Received Arguements for Debugging
+		System.out.println(s);//Printing Received Arguements for Debugging
+
+		//Calling function for Fetching installed Packages 
+		fetch_installed_packages(); //This function will print all installed packages
+
+		
+		
+
+		String clone_path="";//Holds path to  destination Where the Repository Must Be Clonned
+		String ide="";  //Holds command for IDE to open upon
 
 
+		//Fetching Configurations from Config File
+		try{
+		File propertiesFile = new File("config.properties");
+        FileReader reader = new FileReader(propertiesFile);
+        Properties props = new Properties();
+        props.load(reader);
+		String test = props.getProperty("test");
+		System.out.println(test);
+		clone_path= props.getProperty("clonning_dir");//Place Where the Repository Must Be Clonned
+		ide=props.getProperty("ide"); //IDE to open upon
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
 
         //Clonnning Git Repo
         //Expected parameter: https://github.com/user-name/repo-name.git
         String repo_name=args[0].substring(args[0].lastIndexOf("/"),args[0].lastIndexOf(".")); //Extracts the Name of Repository
-        String clone_path= "/home/fahad/MyProjects/starfish_clonned/"; //Place Where the Repository Must Be Clonned
+        //String clone_path= "/home/fahad/MyProjects/starfish_clonned/"; 
         String cmd = "git clone "+args[0];
         String originUrl = args[0];
 		Path directory = Paths.get(clone_path+repo_name);
@@ -28,7 +56,7 @@ class starfish{
 
         //Launching Vscode on the Cloned Directory 
         System.out.println("Launching Vscode Now");
-        runCommand(directory.getParent(), "code",clone_path+repo_name);
+        runCommand(directory.getParent(), ide,clone_path+repo_name);
         
     }
 
@@ -88,7 +116,35 @@ class starfish{
 				ioe.printStackTrace();
 			}
 		}
-    }
+	}
+	
+
+	//Function to fetch all installed packages on System
+	public static void fetch_installed_packages()throws Exception{
+		
+		String [] extCmdArgs = new String[]{"dpkg-query", "-W", "-f=${Package} ${Version}\n"};// Fetches installed Packages on System
+		//String [] extCmdArgs = new String[]{"sudo apt list --installed};
+		
+		Process fetchInstalledPackagesProcess = Runtime.getRuntime().exec(extCmdArgs);
+		BufferedReader reader = null;
+				try {
+					reader = new BufferedReader(new InputStreamReader(fetchInstalledPackagesProcess.getInputStream()));
+					String line;
+					while(true){
+						line = reader.readLine();
+						if (line == null)
+							break;
+						System.out.println(line);
+				}
+			} catch (Exception e) {
+					e.printStackTrace();
+			}
+			finally {
+				reader.close();
+			}
+
+
+	}
     
 
  
