4b8d28e94616e74232938b15c7030f0cc3cbd3ee
fahad-israr <fahad00cms@gmail.com>
2020-06-19 04:49:13 +0530

    Binary building with project structure initiated

---
+++ src/main/java/dk/xam/starfish/JavaMain.java
@@ -0,0 +1,11 @@
+package dk.xam.starfish;
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class JavaMain {
+
+    public static void main(String... args) {
+        Quarkus.run(Starfish.class, args);
+    }
+}
